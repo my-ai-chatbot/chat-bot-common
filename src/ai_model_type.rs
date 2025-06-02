47149a0e203e4cc52f65465715f4a9d6b2d2291a
amigin <amigin@gmail.com>
2025-06-02 16:50:50 +0300

    Added AI Models

---
+++ src/ai_model_type.rs
@@ -0,0 +1,5 @@
+pub const GPT_4O: &'static str = "gpt-4o";
+pub const GWEN3_32B: &'static str = "qwen3:32b";
+pub const GWEN3_14B: &'static str = "qwen3:14b";
+
+pub static AI_MODELS: &[&'static str] = &[GPT_4O, GWEN3_32B, GWEN3_14B];
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod ai_model_type;
 pub mod browsers;
 pub mod languages;
 pub mod request_metadata;
