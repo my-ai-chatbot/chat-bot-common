cf0b07e3e8ae06896fc35872209467553c61880d
amigin <amigin@gmail.com>
2025-06-08 14:46:01 +0300

    Added AuditLogChatModel

--- src/audit_log.rs
+++ src/audit_log.rs
@@ -1,1 +1,35 @@
+use rust_extensions::date_time::DateTimeAsMicroseconds;
+use serde::*;
+
 pub const AUDIT_LOG_CHAT_EVENT: &'static str = "chat-event";
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Default, Clone)]
+pub struct AuditLogChatModel {
+    pub client_question: String,
+    pub llm_answer: String,
+    pub ai_agent_duration: Option<String>,
+
+    pub stt_audio_file: Option<String>,
+    pub stt_duration: Option<String>,
+    pub stt: Option<String>,
+
+    pub req_time: Option<String>,
+
+    pub tts_audio_file: Option<String>,
+    pub tts_duration: Option<String>,
+    pub tts: Option<String>,
+
+    pub resp_time: Option<String>,
+}
+
+impl AuditLogChatModel {
+    pub fn get_req_time(&self) -> Option<DateTimeAsMicroseconds> {
+        let req_time = self.req_time.as_ref()?;
+        DateTimeAsMicroseconds::from_str(req_time)
+    }
+
+    pub fn get_resp_time(&self) -> Option<DateTimeAsMicroseconds> {
+        let resp_time = self.resp_time.as_ref()?;
+        DateTimeAsMicroseconds::from_str(resp_time)
+    }
+}
