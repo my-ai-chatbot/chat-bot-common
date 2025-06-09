d113af1baaeaacf5371f889969aff141905fe59d
amigin <amigin@gmail.com>
2025-06-09 16:05:55 +0300

    Fixes

--- src/ai_model_type.rs
+++ src/ai_model_type.rs
@@ -1,7 +1,8 @@
 pub const GPT_4O: &'static str = "gpt-4o";
 pub const GPT_4O_API: &'static str = "gpt-4o-api";
 pub const GWEN3_32B: &'static str = "qwen3:32b";
+pub const TEST: &'static str = "test";
 
-pub static AI_MODELS: &[&'static str] = &[GPT_4O, GPT_4O_API, GWEN3_32B];
+pub static AI_MODELS: &[&'static str] = &[GPT_4O, GPT_4O_API, GWEN3_32B, TEST];
 
 pub const DEFAULT: &'static str = GPT_4O;
