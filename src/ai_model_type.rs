0c2d52a4cdb4e7c64faf244b4a799f95f8a9f9e3
amigin <amigin@gmail.com>
2025-06-09 09:01:15 +0300

    Added Gwen as a model

--- src/ai_model_type.rs
+++ src/ai_model_type.rs
@@ -1,6 +1,7 @@
 pub const GPT_4O: &'static str = "gpt-4o";
 pub const GPT_4O_API: &'static str = "gpt-4o-api";
+pub const GWEN3_32B: &'static str = "qwen3:32b";
 
-pub static AI_MODELS: &[&'static str] = &[GPT_4O, GPT_4O_API];
+pub static AI_MODELS: &[&'static str] = &[GPT_4O, GPT_4O_API, GWEN3_32B];
 
 pub const DEFAULT: &'static str = GPT_4O;
