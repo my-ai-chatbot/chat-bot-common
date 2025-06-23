c477458c2b4fb4f51b86c6d0411d45c08fd923e2
amigin <amigin@gmail.com>
2025-06-23 14:29:24 +0300

    Minor Improvements

--- src/stt_options.rs
+++ src/stt_options.rs
@@ -1,9 +1,22 @@
 pub const SST_ELEVEN_LABS: &'static str = "11labs";
 pub const SST_KYUTAI: &'static str = "kyutai";
+pub const SST_MUNSIT: &'static str = "munsit";
 
 pub enum SttOption {
     ElevenLabs,
     Kyutai,
+    Munsit,
+}
+
+impl SttOption {
+    pub fn from_str(src: &str) -> Self {
+        match src {
+            SST_ELEVEN_LABS => Self::ElevenLabs,
+            SST_KYUTAI => Self::Kyutai,
+            SST_MUNSIT => Self::Munsit,
+            _ => Default::default(),
+        }
+    }
 }
 
 impl Default for SttOption {
@@ -12,4 +25,4 @@ impl Default for SttOption {
     }
 }
 
-pub const ALL: &[&'static str] = &[SST_ELEVEN_LABS, SST_KYUTAI];
+pub const ALL: &[&'static str] = &[SST_ELEVEN_LABS, SST_KYUTAI, SST_MUNSIT];
