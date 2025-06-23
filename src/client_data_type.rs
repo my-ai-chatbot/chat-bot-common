210918172a21028d46b53f613c71ae126c57e6b7
amigin <amigin@gmail.com>
2025-06-23 13:35:34 +0300

    Naming Fixes

--- src/client_data_type.rs
+++ src/client_data_type.rs
@@ -1,3 +1,18 @@
+pub enum ClientDataType {
+    MinaghiAuto,
+    DarGlobalRealEstate,
+}
+
+impl ClientDataType {
+    pub fn from_str(src: &str) -> Option<Self> {
+        match src {
+            MINAGHI_AUTO => Self::MinaghiAuto.into(),
+            DG_REAL_ESTATE => Self::DarGlobalRealEstate.into(),
+            _ => None,
+        }
+    }
+}
+
 pub const MINAGHI_AUTO: &'static str = "minaghi-auto";
 pub const DG_REAL_ESTATE: &'static str = "dg-real-estate";
 
