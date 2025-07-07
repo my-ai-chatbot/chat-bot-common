cb68534dec6f5a90362e3dfd758ff6cdd96f6aeb
amigin <amigin@gmail.com>
2025-07-07 09:41:20 +0300

    Amenities are added

--- src/dg_tool_calls_models/project_json_model.rs
+++ src/dg_tool_calls_models/project_json_model.rs
@@ -8,4 +8,6 @@ pub struct ProjectJsonModel {
     #[serde(rename = "type")]
     pub project_type: String,
     pub project_url: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub amenities: Option<Vec<String>>,
 }
