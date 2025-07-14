0e12d839d4bc7d5556380c921bf354bacb2d39d7
amigin <amigin@gmail.com>
2025-07-14 16:11:42 +0300

    Added SalesOfficeJsonModel

--- src/dg_tool_calls_models/mod.rs
+++ src/dg_tool_calls_models/mod.rs
@@ -2,3 +2,5 @@ mod project_json_model;
 pub use project_json_model::*;
 mod property_unit_model;
 pub use property_unit_model::*;
+mod sales_office;
+pub use sales_office::*;
---
+++ src/dg_tool_calls_models/sales_office.rs
@@ -0,0 +1,9 @@
+use serde::*;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct SalesOfficeJsonModel {
+    pub city: String,
+    pub addresses: Vec<String>,
+    pub phones: Vec<String>,
+    pub img_url: Option<String>,
+}
