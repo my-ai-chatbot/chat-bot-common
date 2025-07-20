c4b299ec86f055f4235e12e74c7a7e2ebd214356
amigin <amigin@gmail.com>
2025-07-20 14:30:44 +0300

    Currency Converter is now Struct

--- src/currency_converter.rs
+++ src/currency_converter.rs
@@ -1,3 +1,45 @@
-pub trait CurrencyConverter {
-    fn convert_to_usd(&self, amount: f64, currency_code: &str) -> Option<f64>;
+#[derive(Debug, Clone)]
+pub struct CurrencyRate {
+    pub from_currency: String,
+    pub to_currency: String,
+    pub rate: f64,
+}
+
+pub struct CurrencyConverter {
+    data: Vec<CurrencyRate>,
+}
+
+impl CurrencyConverter {
+    pub fn new() -> Self {
+        Self { data: Vec::new() }
+    }
+
+    pub fn convert(&self, from_currency: &str, to_currency: &str, amount: f64) -> Option<f64> {
+        for itm in &self.data {
+            if itm.from_currency == from_currency && itm.to_currency == to_currency {
+                return Some(amount * itm.rate);
+            }
+
+            if itm.from_currency == to_currency && itm.to_currency == from_currency {
+                return Some(amount / itm.rate);
+            }
+        }
+
+        None
+    }
+
+    pub fn update_rate(&mut self, rate: CurrencyRate) {
+        let index = self.data.iter().position(|itm| {
+            itm.from_currency == rate.from_currency && itm.to_currency == rate.to_currency
+        });
+
+        match index {
+            Some(index) => {
+                self.data.get_mut(index).unwrap().rate = rate.rate;
+            }
+            None => {
+                self.data.push(rate);
+            }
+        }
+    }
 }
