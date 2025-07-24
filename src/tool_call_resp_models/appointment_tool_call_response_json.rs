272a77e9ed4b9c9e2446f6106c7a29d0ad2b8081
amigin <amigin@gmail.com>
2025-07-24 18:39:27 +0300

    Added appointment_date and time

--- src/tool_call_resp_models/appointment_tool_call_response_json.rs
+++ src/tool_call_resp_models/appointment_tool_call_response_json.rs
@@ -2,5 +2,6 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct PropertyToolCallResponseJsonModel {
-    pub appointment_time_zone: String,
+    pub appointment_date: String,
+    pub appointment_time: String,
 }
