8a6c7236164a94168869d8a56e5803bed6392208
amigin <amigin@gmail.com>
2025-07-24 18:42:17 +0300

    Fixes

--- src/tool_call_resp_models/appointment_tool_call_response_json.rs
+++ src/tool_call_resp_models/appointment_tool_call_response_json.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct PropertyToolCallResponseJsonModel {
+pub struct AppointmentToolCallResponseJsonModel {
     pub appointment_date: String,
     pub appointment_time: String,
 }
