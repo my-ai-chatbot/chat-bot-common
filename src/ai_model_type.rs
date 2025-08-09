e710ff47a4fb80eff77a28b3a6f785069f66a2b2
amigin <amigin@gmail.com>
2025-08-09 11:25:32 +0300

    Added GPT-5

--- src/ai_model_type.rs
+++ src/ai_model_type.rs
@@ -1,7 +1,8 @@
 pub const GPT_4O: &'static str = "gpt-4o";
-pub const GWEN3_32B: &'static str = "qwen3:32b";
-pub const TEST: &'static str = "test";
+pub const GPT_5: &'static str = "gpt-5";
 
-pub static AI_MODELS: &[&'static str] = &[GPT_4O, GWEN3_32B, TEST];
+pub const GWEN3_32B: &'static str = "qwen3:32b";
+
+pub static AI_MODELS: &[&'static str] = &[GPT_4O, GPT_5];
 
 pub const DEFAULT: &'static str = GPT_4O;
