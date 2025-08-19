24f1f3a55a52c3c073db973cd4c1186413eb0b99
amigin <amigin@gmail.com>
2025-08-19 13:26:00 +0300

    Added  Qwen3_30b_a3b

--- src/ai_model_type.rs
+++ src/ai_model_type.rs
@@ -1,8 +1,8 @@
 pub const GPT_4O: &'static str = "gpt-4o";
 pub const GPT_5: &'static str = "gpt-5";
 
-pub const GWEN3_32B: &'static str = "qwen3:32b";
+pub const QWEN3_30B_A3B: &'static str = "Qwen3-30B-A3B";
 
-pub static AI_MODELS: &[&'static str] = &[GPT_4O, GPT_5];
+pub static AI_MODELS: &[&'static str] = &[GPT_4O, GPT_5, QWEN3_30B_A3B];
 
 pub const DEFAULT: &'static str = GPT_4O;
