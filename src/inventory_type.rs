d20a697038c7f5a7f0e1e0fa9447df10a3a87c4b
amigin <amigin@gmail.com>
2025-08-27 14:30:01 +0300

    Fixes with InventoryType

---
+++ src/inventory_type.rs
@@ -0,0 +1,34 @@
+pub enum InventoryType {
+    MinaghiAuto,
+    DarGlobalRealEstate,
+    SalesTeq,
+}
+
+impl InventoryType {
+    pub fn from_str(src: &str) -> Option<Self> {
+        match src {
+            MINAGHI_AUTO => Self::MinaghiAuto.into(),
+            DG_REAL_ESTATE => Self::DarGlobalRealEstate.into(),
+            SALESTEQ => Self::SalesTeq.into(),
+            _ => None,
+        }
+    }
+
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Self::MinaghiAuto => MINAGHI_AUTO,
+            Self::DarGlobalRealEstate => DG_REAL_ESTATE,
+            Self::SalesTeq => SALESTEQ,
+        }
+    }
+}
+
+pub const ALL: &[InventoryType] = &[
+    InventoryType::MinaghiAuto,
+    InventoryType::DarGlobalRealEstate,
+    InventoryType::SalesTeq,
+];
+
+const MINAGHI_AUTO: &'static str = "minaghi-auto";
+const DG_REAL_ESTATE: &'static str = "dg-real-estate";
+const SALESTEQ: &'static str = "salesteq";
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 pub mod ai_model_type;
 pub mod audit_log;
 pub mod browsers;
-pub mod client_data_type;
 pub mod currency_converter;
 pub mod dg_tool_calls_models;
+pub mod inventory_type;
 pub mod languages;
 pub mod request_metadata;
 pub mod stt_options;
