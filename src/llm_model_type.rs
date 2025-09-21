920c887fb5412ea58f540a8b867b5c9a2aa3e877
amigin <amigin@gmail.com>
2025-09-21 17:15:30 +0300

    Added Gpt5 Mini and Nano

--- src/llm_model_type.rs
+++ src/llm_model_type.rs
@@ -5,6 +5,8 @@ use crate::*;
 pub const GPT_4O: &'static str = "gpt-4o";
 pub const GPT_4O_MINI: &'static str = "gpt-4o-mini";
 pub const GPT_5: &'static str = "gpt-5";
+pub const GPT_5_MINI: &'static str = "gpt-5-mini";
+pub const GPT_5_NANO: &'static str = "gpt-5-nano";
 pub const QWEN3_30B_A3B: &'static str = "Qwen3-30B-A3B";
 
 #[derive(Debug, Clone, Copy, Serialize, Deserialize, PartialEq, Eq, Hash, PartialOrd, Ord)]
@@ -12,6 +14,8 @@ pub enum ChatBotLlmModel {
     Gpt4o,
     Gpt4oMini,
     Gpt5,
+    Gpt5Mini,
+    Gpt5Nano,
     Qwen3_30BA3n,
 }
 
@@ -22,14 +26,22 @@ impl Default for ChatBotLlmModel {
 }
 
 impl ChatBotLlmModel {
-    pub const ALL: &'static [Self] =
-        &[Self::Gpt4o, Self::Gpt4oMini, Self::Gpt5, Self::Qwen3_30BA3n];
+    pub const ALL: &'static [Self] = &[
+        Self::Gpt4o,
+        Self::Gpt4oMini,
+        Self::Gpt5,
+        Self::Gpt5Mini,
+        Self::Gpt5Nano,
+        Self::Qwen3_30BA3n,
+    ];
 
     pub fn try_from_str(src: &str) -> Option<Self> {
         match src {
             GPT_4O => Some(Self::Gpt4o),
             GPT_4O_MINI => Some(Self::Gpt4oMini),
             GPT_5 => Some(Self::Gpt5),
+            GPT_5_MINI => Some(Self::Gpt5Mini),
+            GPT_5_NANO => Some(Self::Gpt5Nano),
             QWEN3_30B_A3B => Some(Self::Qwen3_30BA3n),
             _ => None,
         }
@@ -40,6 +52,8 @@ impl ChatBotLlmModel {
             Self::Gpt4o => GPT_4O,
             Self::Gpt4oMini => GPT_4O_MINI,
             Self::Gpt5 => GPT_5,
+            Self::Gpt5Mini => GPT_5_MINI,
+            Self::Gpt5Nano => GPT_5_NANO,
             Self::Qwen3_30BA3n => QWEN3_30B_A3B,
         }
     }
