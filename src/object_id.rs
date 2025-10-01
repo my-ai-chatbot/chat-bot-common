8bd640319a747d83791d98f0480b5b3a7e2322fe
amigin <amigin@gmail.com>
2025-10-01 12:21:23 +0300

    Improvements

--- src/object_id.rs
+++ src/object_id.rs
@@ -78,6 +78,10 @@ impl ObjectId {
         self.0 == other.0
     }
 
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+
     pub fn len(&self) -> usize {
         self.0.len()
     }
--- src/object_id_static.rs
+++ src/object_id_static.rs
@@ -61,6 +61,10 @@ impl ObjectIdStatic {
     pub fn len(&self) -> usize {
         self.0.len()
     }
+
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
 }
 
 impl Default for ObjectIdStatic {
