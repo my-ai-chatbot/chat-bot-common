1fcfc3e6589b4798f8b3acd25b720d2047104ca1
amigin <amigin@gmail.com>
2025-10-02 10:36:11 +0300

    Fixes

--- src/object_id/as_str.rs
+++ src/object_id/as_str.rs
@@ -1,3 +1,21 @@
 pub trait AsStr {
     fn as_str(&self) -> &str;
 }
+
+impl AsStr for String {
+    fn as_str(&self) -> &str {
+        self.as_str()
+    }
+}
+
+impl AsStr for &'_ str {
+    fn as_str(&self) -> &str {
+        self
+    }
+}
+
+impl AsStr for &'_ String {
+    fn as_str(&self) -> &str {
+        self
+    }
+}
