d3608ea07c54aa96b195ad1c829fbd28ee630392
amigin <amigin@gmail.com>
2025-10-02 11:00:42 +0300

    Fixes

--- src/object_id/value_validator.rs
+++ src/object_id/value_validator.rs
@@ -6,20 +6,20 @@ pub trait IdExtension {
     fn validate_value(src: &str) -> Option<bool>;
 }
 
-impl IdExtension for String {
-    fn validate_value(_: &str) -> Option<bool> {
+impl ValueValidator for String {
+    fn validate_value(&self) -> Option<bool> {
         None
     }
 }
 
-impl IdExtension for &'_ str {
-    fn validate_value(_: &str) -> Option<bool> {
+impl ValueValidator for &'_ str {
+    fn validate_value(&self) -> Option<bool> {
         None
     }
 }
 
-impl IdExtension for &'_ String {
-    fn validate_value(_: &str) -> Option<bool> {
+impl ValueValidator for &'_ String {
+    fn validate_value(&self) -> Option<bool> {
         None
     }
 }
