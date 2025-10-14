6a92cc0d7beab96ccb929559ed4a45bd7870b2f3
amigin <amigin@gmail.com>
2025-10-14 10:25:11 +0300

    Added Open-AI

--- src/stt_options.rs
+++ src/stt_options.rs
@@ -7,18 +7,26 @@ use crate::languages::Language;
 pub const STT_ELEVEN_LABS: &'static str = "11labs";
 pub const STT_KYUTAI: &'static str = "kyutai";
 pub const STT_MUNSIT: &'static str = "munsit";
+pub const STT_OPEN_AI: &'static str = "open_ai";
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash, Serialize, Deserialize)]
 pub enum SttOption {
+    OpenAi,
     ElevenLabs,
     Kyutai,
     Munsit,
 }
 
 impl SttOption {
-    pub const ALL: &[Self] = &[SttOption::ElevenLabs, SttOption::Kyutai, SttOption::Munsit];
+    pub const ALL: &[Self] = &[
+        SttOption::OpenAi,
+        SttOption::ElevenLabs,
+        SttOption::Kyutai,
+        SttOption::Munsit,
+    ];
     pub fn try_from_str(src: &str) -> Option<Self> {
         match src {
+            STT_OPEN_AI => Self::OpenAi.into(),
             STT_ELEVEN_LABS => Self::ElevenLabs.into(),
             STT_KYUTAI => Self::Kyutai.into(),
             STT_MUNSIT => Self::Munsit.into(),
@@ -27,16 +35,15 @@ impl SttOption {
     }
 
     pub fn from_str(src: &str, lang: Language) -> Self {
-        match src {
-            STT_ELEVEN_LABS => Self::ElevenLabs,
-            STT_KYUTAI => Self::Kyutai,
-            STT_MUNSIT => Self::Munsit,
-            _ => get_default_by_language(&lang),
+        match Self::try_from_str(src) {
+            Some(value) => value,
+            None => get_default_by_language(&lang),
         }
     }
 
     pub fn as_str(&self) -> &'static str {
         match self {
+            SttOption::OpenAi => STT_OPEN_AI,
             SttOption::ElevenLabs => STT_ELEVEN_LABS,
             SttOption::Kyutai => STT_KYUTAI,
             SttOption::Munsit => STT_MUNSIT,
@@ -52,7 +59,7 @@ impl Default for SttOption {
 
 pub fn get_default_by_language(lang_id: &Language) -> SttOption {
     match lang_id {
-        Language::En => SttOption::ElevenLabs,
+        Language::En => SttOption::OpenAi,
         Language::Ar => SttOption::Munsit,
     }
 }
